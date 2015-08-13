68e74785f0330e594847745de2abc827730ccc83
contexis webdesign <nachricht@contexis.de>
2015-08-13 16:20:58 +0200

    multiple files added

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- TEST FILE FOR TESTING TESTS!!! -->
+<html>
+<head>
+	<?php wp_head(); ?>
+</head>
+	
+<body>
+<h1>Hallo</h1>
+
+</body>
+</html>
---
+++ lib/config.php
@@ -0,0 +1,22 @@
+<?php
+
+// This is the place to add theme-support etc.
+
+
+namespace Contexis\Base\Config;
+
+
+/**
+ * Configuration values
+ */
+if (!defined('WP_ENV')) {
+  // Fallback if WP_ENV isn't defined in your WordPress config
+  // Used in lib/assets.php to check for 'development' or 'production'
+  define('WP_ENV', 'production');
+}
+
+if (!defined('DIST_DIR')) {
+  // Path to the build directory for front-end assets
+  define('DIST_DIR', '/dist/');
+}
+
